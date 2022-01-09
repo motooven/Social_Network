fbee2c1ad9761ee83a841dda0f10de237814b358
motooven <motooven@gmail.com>
2022-01-09 12:03:35 +0300

    v9

--- src/components/Dialogs.jsx
+++ src/components/Dialogs.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import s from './Dialogs.module.css'
+import React from 'react'
+import Logo_02 from "../Logotip/logo_02.jpg"
+import s from "./Content.module.css"
+import Post from "./Post";
 
-const Dialogs = () => {
+const Content = () => {
     return (
-        <div className={s.dialogs}>
-            Dialogs
+        <div className={s.content}>
+            <img src={Logo_02} alt=""/>
+            <div>
+                <div>av + description</div>
+                <div>My posts</div>
+                <textarea></textarea>
+                <button>add post</button>
+                <Post post="post 1" like="like 2"/>
+                <Post post="post 2" like="like 3"/>
+                <Post post="post 3" like="like 4"/>
+            </div>
         </div>
-    );
-};
+    )
+}
 
-export default Dialogs;
+
+export default Content
